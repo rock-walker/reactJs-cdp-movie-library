b21c321f1c4b208372f779aac9bd97f6963abd76
Andrei Melnikau <melnikov.work@gmail.com>
2018-04-15 00:09:15 +0300

    create Component task is prepared; createElement in progres..

---
+++ src/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Components.css';
+
+class SearchBar extends React.Component {
+	render() {
+		return (
+			<div>Hello, world</div>
+		);
+	}
+}
+
+export default SearchBar;
---
+++ src/SearchFilter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Components.css';
+
+class SearchFilter extends React.Component {
+	render() {
+		return React.createElement(
+				'h1',
+				'Hello, world'
+		);
+	}
+}
+export default SearchFilter;
