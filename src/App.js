11881b1efd501c7acefda830a46984505867df7d
Andrei Melnikau <melnikov.work@gmail.com>
2018-04-16 10:39:46 +0300

    replaced html 'header' with react.createComponent 'logo' param in image src still failed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppHeader from './AppHeader';
 import SearchBar from './SearchBar';
 import SearchFilter from './SearchFilter';
 import SearchButton from './SearchButton';
@@ -10,10 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <AppHeader />
         <SearchBar />
         <SearchFilter />
         <SearchButton />
