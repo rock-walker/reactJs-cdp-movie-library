3dac52357b561292ef1effe64b7a46439a252d9e
Andrei Melnikau <melnikov.work@gmail.com>
2018-05-02 00:09:39 +0300

    error boundary

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import AppHeader from './AppHeader';
 import StatusBar from './StatusBar';
 import ResultBody from './ResultBody';
 import AppFooter from './AppFooter';
+import ErrorBoundary from './ErrorBoundary';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <AppHeader />
-        <StatusBar />
-        <ResultBody/>
-        <AppFooter />
+        <ErrorBoundary>
+            <AppHeader />
+            <StatusBar />
+            <ResultBody/>
+            <AppFooter />
+        </ErrorBoundary>
       </div>
     );
   }
