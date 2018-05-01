0c0706a37e30079f6877acf64fb7848d865b3a49
Andrei Melnikau <melnikov.work@gmail.com>
2018-05-01 17:08:13 +0300

    first page markup is ready.

--- src/MovieItem.js
+++ src/MovieItem.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
+import cover from './killbill.jpg';
 import './Components.css';
 
 class MovieItem extends Component {
-	render() {
-		return(
-			<img src={props.coverSrc} />
-			<span>{props.title}</span>
-			<span>{props.rate}</span>
-			<span>{props.duration}</span>
-			<p>{props.description}</p>
-		);
-	}
+    render() {
+        return(
+            <div className="movieItem">
+                <img src={cover}/>
+                <div className="movieDescription">
+                    <span className="title">KILL BILL: VOL.1</span>
+                    <div className="year">2004</div>
+                    <p>Actions</p>
+                </div>
+            </div>
+        );
+    }
 }
+
+export default MovieItem;
--- src/ResultBody.jsx
+++ src/ResultBody.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import './Components.css';
+import MovieItem from './MovieItem';
 
 class ResultBody extends Component {
     render() {
         return(
             <div className="resultBody">
-                
+                <MovieItem />
+                <MovieItem />
+                <MovieItem />
+                <MovieItem />
             </div>
         );
     }
