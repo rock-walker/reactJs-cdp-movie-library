f267108f4dba5776eb404e4e94b2db5bea88d69f
Andrei Melnikau <melnikov.work@gmail.com>
2018-06-23 03:01:04 +0300

    launched first page

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,7 +2,7 @@ import {combineReducers} from 'redux'
 import { all } from 'redux-saga/effects'
 
 import { routerReducer } from 'react-router-redux'
-import { appReducers } from '../reducers'
+import appReducers from '../reducers'
 import { moviesSaga } from '../actions'
 
 function* rootSaga() {
